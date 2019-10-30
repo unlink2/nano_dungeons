a129dfd72d6039617a123f8705e81e7888224259
Lukas Krickl <lukaskrickl@gmail.com>
2019-10-30 12:13:14 +0100

    Updated main menu

--- main.asm
+++ main.asm
@@ -265,7 +265,7 @@ update_done: rti
 
 palette_data:
 .db $0F,$31,$32,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F  ;background palette data
-.db $0F,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C  ;sprite palette data
+.db $0C,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C  ;sprite palette data
 palette_data_end:
 
 ; compressed menu gfx
