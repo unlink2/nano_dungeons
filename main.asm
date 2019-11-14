3b8927c3446a5b5e460988b7c8b388cb7cd2c9d6
Lukas Krickl <lukaskrickl@gmail.com>
2019-11-14 21:35:14 +0100

    Adjusted font

--- main.asm
+++ main.asm
@@ -470,8 +470,8 @@ irq:
 .include "./audio.asm"
 
 palette_data:
-.db $0F,$20,$15,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F  ;background palette data
-.db $0C,$20,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C  ;sprite palette data
+.db $0F,$20,$2D,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F  ;background palette data
+.db $0C,$20,$2D,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C  ;sprite palette data
 palette_data_end:
 
 ; compressed menu gfx
