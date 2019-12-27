59c019b013247cd2341e1ea3d56bc9a248003de1
Lukas Krickl <lukaskrickl@gmail.com>
2019-12-27 09:15:01 +0100

    Added room layout doc

--- genmap.asm
+++ genmap.asm
@@ -10,3 +10,28 @@
 ; 7 surround all floor-empty connections with walls
 ; 8 add random start+end tiles
 ; 9 add random items and enemies
+
+; this sub routine copies a room of a certain id
+; into level_data
+; inputs:
+;   x -> x location
+;   y -> y location
+;   a -> room id from room table
+copy_room:
+    rts
+
+
+; a list of room headers
+; Room header doc:
+;   Each room header consists of 3 bytes
+;   Byte 2: X Size
+;   Byte 1: Y Size
+;   Byte 0: Fill Tile
+rooms_lo:
+.db <test_room
+
+rooms_hi:
+.db >test_room
+
+test_room:
+.db $04, $04, $62
